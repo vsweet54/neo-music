1b305b1db6f85d64889b41f4145ff37df6b8d1dc
vsweet54 <yaniszahiruddin67@gmail.com>
2026-03-17 02:14:01 +0700

    debug minimal

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-process.on('uncaughtException', err => { console.error('UNCAUGHT:', err); process.exit(1); });
-process.on('unhandledRejection', err => { console.error('UNHANDLED:', err); process.exit(1); });
-
+console.log('Step 1');
 require('dotenv').config();
+console.log('Step 2');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
+console.log('Step 3');
 const { DisTube } = require('distube');
-const fs = require('fs');
-const path = require('path');
-
-console.log('Starting bot...');
-
+console.log('Step 4 - distube ok');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -17,39 +13,5 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-
-console.log('Client created');
-
-client.distube = new DisTube(client, {
-  emitNewSongOnly: true,
-  leaveOnEmpty: true,
-  leaveOnFinish: true,
-  nsfw: false,
-});
-
-console.log('DisTube created');
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(f => f.endsWith('.js'));
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  if (command.data && command.execute) client.commands.set(command.data.name, command);
-}
-
-console.log('Commands loaded');
-
-const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(f => f.endsWith('.js'));
-for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-  if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
-  else client.on(event.name, (...args) => event.execute(...args, client));
-}
-
-const distubeEventFiles = fs.readdirSync(path.join(__dirname, 'events/distube')).filter(f => f.endsWith('.js'));
-for (const file of distubeEventFiles) {
-  const event = require(`./events/distube/${file}`);
-  client.distube.on(event.name, (...args) => event.execute(...args, client));
-}
-
-console.log('Logging in...');
+console.log('Step 5 - client ok');
 client.login(process.env.DISCORD_TOKEN);
